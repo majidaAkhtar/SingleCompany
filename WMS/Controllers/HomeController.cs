5b9b04ee94814bcd08d327f20c26465f39485c73
asad <asadullahbeg@gmail.com>
2015-09-02 12:03:49 +0500

    SDSS

--- WMS/Controllers/HomeController.cs
+++ WMS/Controllers/HomeController.cs
@@ -97,13 +97,13 @@ namespace WMS.Controllers
         {
             try
             {
-                //using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "fatima-group.com"))
-                //{
-                //  //validate the credentials
-                // //bool isValid = pc.ValidateCredentials("ffl.ithelpdesk", "fatima@0202");
-                //  bool isValid = pc.ValidateCredentials(u.UserName, u.Password);
-                //if (isValid)
-                // {
+                using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "fatima-group.com"))
+                {
+                  //validate the credentials
+                 //bool isValid = pc.ValidateCredentials("ffl.ithelpdesk", "fatima@0202");
+                  bool isValid = pc.ValidateCredentials(u.UserName, u.Password);
+                if (isValid)
+                 {
                         if (ModelState.IsValid) // this is check validity
                         {
                             using (TAS2013Entities dc = new TAS2013Entities())
@@ -164,9 +164,9 @@ namespace WMS.Controllers
                                 }
                             }
                         }
-                 // }
+                  }
                 return RedirectToAction("index");
-             //   }
+                }
 
 
                 //using (var context = new PrincipalContext(ContextType.Domain, "fatima-group.com", "ffl.ithelpdesk@fatima-group.com", "fatima@0202"))
