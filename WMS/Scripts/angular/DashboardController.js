a17a78f38e9b81ccf68967d2c3a80e34a6072d59
asad <asadullahbeg@gmail.com>
2015-09-28 00:35:56 +0500

    Added the first working prototype dashboard pie graph for dailysummary. it can show pie chart with any type of criteria

--- WMS/Scripts/angular/DashboardController.js
+++ WMS/Scripts/angular/DashboardController.js
@@ -1,52 +1,134 @@
-﻿app.controller('DashboardController', function ($scope) {
+﻿app.controller('DashboardController', function ($scope, $http) {
+    //for popluating the Date input with the current date
+    var today = new Date();
+    $scope.DateFrom = today;
+    //array with the criteria names
+    $scope.Criteria = {
+        repeatSelect: null,
+        availableOptions: [
+          { id: 'C', name: 'Company' },
+          { id: 'L', name: 'Location' },
+          { id: 'D', name: 'Department' },
+          { id: 'E', name: 'Section' },
+          { id: 'S', name: 'Shift' },
+          { id: 'A', name: 'Category' }
+        ]
+    };
+    //array with the values once the criteria is selected
+    $scope.Value = {
+        repeatSelect: null,
+        availableOptions:[]
+    
+    };
+    //This function calls to the database to give the values stored in it 
+    //by making a SUmmaryDataCriteria in the frontend
+    $scope.RenderGraph = function () {
 
-    $scope.addPoints = function () {
-        var seriesArray = $scope.highchartsNG.series
-        var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
+        console.log($scope.Value.repeatSelect);
+        $scope.finalCriteriaForDB = ('' + $scope.DateFrom.getFullYear()).slice(-2) + ('0' + ($scope.DateFrom.getMonth() + 1)).slice(-2) + ('0' + $scope.DateFrom.getDate()).slice(-2) + $scope.Criteria.repeatSelect + $scope.Value.repeatSelect;
+        console.log($scope.finalCriteriaForDB);
+        $http({ method: 'POST', url: '/Home/GetGraphValues', data: JSON.stringify({ CriteriaValue: $scope.finalCriteriaForDB }) }).
+   then(function (response) {
+       ReRenderGraphInfo(response.data);
+       
+   }, function (response) {
+       // called asynchronously if an error occurs
+       // or server returns response with an error status.
+   });
+
+
     };
 
-    $scope.addSeries = function () {
-        var rnd = []
-        for (var i = 0; i < 10; i++) {
-            rnd.push(Math.floor(Math.random() * 20) + 1)
-        }
-        $scope.highchartsNG.series.push({
-            data: rnd
-        })
-    }
+    //This function takes the values given back by the database after selecting everything
+    //in the front end like criteria, date, value
+    var ReRenderGraphInfo = function (graphdata)
+    {
+        console.log(graphdata.AbsentEmps);
+        $scope.highchartsNG.series = [{
+            name: "Attendence",
+            colorByPoint: true,
+            data: [{
+                name: "Absent Employees",
+                y: graphdata.AbsentEmps
+            }, {
+                name: "Present Employees",
+                y: graphdata.PresentEmps,
+                sliced: true,
+                selected: true
+            }, {
+                name: "On Leave",
+                y: graphdata.LvEmps + graphdata.ShortLvEmps + graphdata.HalfLvEmps
+            }, {
+                name: "Day Off",
+                y: graphdata.DayOffEmps
+            }]
+        }];
+        $scope.highchartsNG.loading = false;
 
-    $scope.removeRandomSeries = function () {
-        var seriesArray = $scope.highchartsNG.series
-        var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        seriesArray.splice(rndIdx, 1)
-    }
-
-    $scope.options = {
-        type: 'line'
-    }
-
-    $scope.swapChartType = function () {
-        if (this.highchartsNG.options.chart.type === 'line') {
-            this.highchartsNG.options.chart.type = 'bar'
-        } else {
-            this.highchartsNG.options.chart.type = 'line'
-        }
-    }
-
+    };
+    //Once the criteria changes a backend call is made which gets all the unique values of the 
+    //selected criteria from the dailysummary table. e.g. if we select C all the  companies will
+    //be rendered in the frontend but the  companies wont be duplicated. although in the dailysummary
+    //table there are duplicates for the same company the backend will return all the unique company
+     //values which are stored in the daily summary
+    $scope.$watch('Criteria', function () {
+        var GraphClass = { Criteria: $scope.Criteria.repeatSelect };
+        
+       
+        $http({ method: 'POST', url: '/Home/GetCriteriaNames', data: JSON.stringify(GraphClass) }).
+   then(function (response) {
+       $scope.Value.availableOptions = response.data;
+   }, function (response) {
+       // called asynchronously if an error occurs
+       // or server returns response with an error status.
+   });
+    },true);
+    
+    
     $scope.highchartsNG = {
         options: {
             chart: {
-                type: 'bar'
+                type: 'pie'
+            }
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
             }
         },
         series: [{
-            data: [10, 15, 12, 8, 7]
+            name: "Attendence",
+            colorByPoint: true,
+            data: [{
+                name: "Absent Employees",
+                y: 56.33
+            }, {
+                name: "Present Employees",
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: "On Leave",
+                y: 10.38
+            }, {
+                name: "Day Off",
+                y: 4.77
+            }]
         }],
         title: {
-            text: 'Hello'
+            text: 'Daily Summary'
         },
-        loading: false
+        //once we have fetched the info from the database we will do loading :false
+        loading: true
     }
+   
 
 });
